5c9a1ed64f4f158b866eff04d1940b5d35197fe5
jinju2828 <jinju2828@gmail.com>
2019-05-03 11:16:29 -0700

    counter with redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Button from './components/Button';
+import Counter from './components/Counter';
+import Option from './components/Option';
+
+class App extends Component {
+  render(){
+    return(
+        <div>
+          <Counter store = {this.props.store} />
+          <Option store = {this.props.store} />
+          <Button store = {this.props.store} />
+        </div>
+    );
+  }
 }
 
 export default App;
